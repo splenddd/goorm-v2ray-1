9992020540918ea3595ce8393c35435f39a769ac
splenddd <56500306+splenddd@users.noreply.github.com>
2021-06-27 12:19:13 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -4,17 +4,17 @@ mkdir /v2ray
 cd /v2ray
 rm -f /v2ray/v2ray
 #wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ray
-wget https://github.com/splenddd/goorm-v2ray-1/blob/master/v2ray
+wget https://raw.githubusercontent.com/splenddd/goorm-v2ray-1/master/v2ray
 
 #设置运行权限
 chmod +x /v2ray/v2ray
 rm -f /v2ray/v2ctl
 #wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ctl
-wget https://github.com/splenddd/goorm-v2ray-1/blob/master/v2ctl
+wget https://raw.githubusercontent.com/splenddd/goorm-v2ray-1/master/v2ctl
 
 chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
 #wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/config.json
-wget https://github.com/splenddd/goorm-v2ray-1/blob/master/config.json
+wget https://raw.githubusercontent.com/splenddd/goorm-v2ray-1/master/config.json
 # 开始后台运行
 nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
